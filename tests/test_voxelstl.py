e01112d1f89e942587f550ee6981c557ec438f7b
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2017-07-11 11:25:19 +0200

    use numpy-stl for testing instead of ngsolve

--- tests/test_voxelstl.py
+++ tests/test_voxelstl.py
@@ -5,6 +5,9 @@ from pyspdlog import *
 from struct import pack
 from netgen.stl import *
 from numpy import pi
+import stl
+from matplotlib import pyplot
+from mpl_toolkits import mplot3d
 
 def CreateVoxel(filename,indicator_function,dimensions):
     l = []
@@ -38,16 +41,19 @@ def test_voxelSTL():
     geo.ApplySmoothingStep(False)
     logger.info("Smoothing done, start writing to stl file")
     geo.WriteSTL("model.stl")
-    netgen_geo = LoadSTLGeometry("model.stl")
-    mesh = Mesh(netgen_geo.GenerateMesh())
-    #mesh.Curve(4)
+    stl_mesh = stl.mesh.Mesh.from_file("model.stl")
     if __name__ == "__main__":
-        return mesh
-    assert abs(Integrate(CoefficientFunction(1),mesh)- 4*pi/3*0.001/4) < 2e-4
+        figure = pyplot.figure()
+        axes = mplot3d.Axes3D(figure)
+        axes.add_collection3d(mplot3d.art3d.Poly3DCollection(stl_mesh.vectors))
+        scale = stl_mesh.points.flatten(-1)
+        axes.auto_scale_xyz(scale,scale,scale)
+        pyplot.show()
+    volume, cog, inertia = stl_mesh.get_mass_properties()
+    print("Volume: ", volume)
+    print("Cog: ", cog)
+    print("Inertia: ", inertia)
+    assert abs(volume - 4*pi/3*0.001/4) < 2e-4
 
 if __name__ == "__main__":
-    mesh = test_voxelSTL()
-    integral = Integrate(CoefficientFunction(1),mesh)
-    print("Integral: ", integral)
-    print("Difference: ",abs(integral - 4*pi/3*0.001/4))
-    Draw(mesh)
+    test_voxelSTL()
